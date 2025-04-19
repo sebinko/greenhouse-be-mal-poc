491dff672c7b2332ee38ccab5e17952d20db573d
sebinko <halas.sebastian@gmail.com>
2025-04-19 21:30:54 +0200

    little refactor

--- src/IoTConsumer/Services/MqttConsumerHostedService.cs
+++ src/IoTConsumer/Services/MqttConsumerHostedService.cs
@@ -5,7 +5,7 @@ using System;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace IoTConsumer
+namespace IoTConsumer.Services
 {
     public class MqttConsumerHostedService : IHostedService
     {
